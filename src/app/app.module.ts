7cf2ac274e9df2b64fa0287955cc423bd1c9b8fa
TrongAnh <tronganhnguyen222@gmail.com>
2020-08-05 13:24:59 +0700

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
